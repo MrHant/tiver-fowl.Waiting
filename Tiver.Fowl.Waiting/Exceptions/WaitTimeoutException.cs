d659ec1f274e6c4bca0f48a7b200c1ddc28c26e3
Artyom Silivonchik <mr.hant@gmail.com>
2017-11-15 08:26:01 +0300

    Remove extra WaitTimeoutException constructors

--- Tiver.Fowl.Waiting/Exceptions/WaitTimeoutException.cs
+++ Tiver.Fowl.Waiting/Exceptions/WaitTimeoutException.cs
@@ -6,22 +6,8 @@
     [Serializable]
     public class WaitTimeoutException : Exception
     {
-        public WaitTimeoutException()
-        {
-        }
-
-        public WaitTimeoutException(string message)
-            : base(message)
-        {
-        }
-
         public WaitTimeoutException(string message, Exception inner)
             : base(message, inner)
-        {
-        }
-
-        protected WaitTimeoutException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
