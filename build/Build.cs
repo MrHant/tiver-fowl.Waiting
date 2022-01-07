d9be9da2e96a766fa4e1a5ce366d37632e31486c
Artsiomi Silivonchyk <mr.hant@gmail.com>
2022-01-07 23:34:14 +0300

    Disable unsupported Tests SetLogger in nuke 6

--- build/Build.cs
+++ build/Build.cs
@@ -76,7 +76,7 @@ class Build : NukeBuild
             DotNetTest(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
-                .SetLogger("Appveyor")
+                //.SetLogger("Appveyor")
                 .SetResultsDirectory(ArtifactsDirectory)
             );
         });
